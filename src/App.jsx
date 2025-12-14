3ac416da92a87afa3ee32466b96ab354fbb0e104
Lauren Lyons <lnl0017@auburn.edu>
2025-12-14 00:33:27 -0600

    Flipping mechanic corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { GameHeader } from './components/GameHeader'
 import { Card } from './components/Card';
 
@@ -6,15 +6,83 @@ import { Card } from './components/Card';
 const cardValues = ["🦁","🐯","🦝","🐷","🐮","🐻‍❄️","🐸","🐻","🦁","🐯","🦝","🐷","🐮","🐻‍❄️","🐸","🐻"]
 function App() {
 
+  const [cards, setCards] = useState([]);
+  const [flippedCards, setFlippedCards] = useState([]);
+
+
+  const initializeGame = () => {
+    // Shuffle Cards
+
+    setCards(cardValues.map((value, index) => (
+      {
+        id: index,
+        value,
+        isFlipped: false,
+        isMatched: false,
+      }
+    )));
+  };
+
+  // usually we will run effect whenever the items within the array after is changed, but giving it an empty array will make it only run when the page is loaded
+  useEffect(() => {
+    initializeGame();
+  }, []);
+
+  const handleCardClick = (card) => {
+    // dont allow clicking if card is already flipped or matched
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+    
+    // Update card flipped state
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return {...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+
+    const newFlippedCards = [...flippedCards, card.id]
+    setFlippedCards(newFlippedCards);
+
+    if (flippedCards.length == 1) {
+      const firstCard = cards[flippedCards[0]];
+      if (firstCard.value === card.value){
+        alert("Matched");
+      } else {
+        // flip back card 1 and 2
+        
+        // allows us to run a function after a certain amount of time
+        setTimeout(() => {
+          const flippedBackCard = newCards.map((c) => {
+            if (newFlippedCards.includes(c.id) || c.id === card.id) {
+              return {...c, isFlipped: false};
+            } else {
+              return c;
+            }
+          });
+          setCards(flippedBackCard)
+
+          setFlippedCards([]);
+        }, 1000); // delay of 1 sec (1000ms)
+
+        
+      }
+    }
+  };
+
   return <div className="app">
     <GameHeader score={3} moves={1}/>
 
     <div className="cards-grid">
-      {cardValues.map((card) => (
-        <Card card={card}></Card>
+      {cards.map((card) => (
+        <Card card={card} onClick={handleCardClick}></Card>
       ))}
     </div>
   </div>;
-}
+};
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
-export const Card = ({card}) => {
-    return <div className="card">
+export const Card = ({ card, onClick }) => {
+    return <div className={`card ${card.isFlipped ? "flipped" : ""}`} onClick={() => onClick(card)}>
         <div className="card-front">?</div>
-        <div className="card-back">{card}</div>
+        <div className="card-back">{card.value}</div>
     </div>
 }
